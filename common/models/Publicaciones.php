4ba69406d29b86870ad4b05dcdc5c796a1100b8d
mavalroot <mavalrodev@gmail.com>
2018-03-12 20:03:30 +0100

    CRUD de Publicaciones generado con gii

---
+++ common/models/Publicaciones.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "publicaciones".
+ *
+ * @property int $id
+ * @property int $usuario_id
+ * @property string $contenido
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Comentarios[] $comentarios
+ * @property User $usuario
+ */
+class Publicaciones extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'publicaciones';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['usuario_id'], 'required'],
+            [['usuario_id'], 'default', 'value' => null],
+            [['usuario_id'], 'integer'],
+            [['contenido'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['usuario_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['usuario_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'usuario_id' => 'Usuario ID',
+            'contenido' => 'Contenido',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComentarios()
+    {
+        return $this->hasMany(Comentarios::className(), ['publicacion_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsuario()
+    {
+        return $this->hasOne(User::className(), ['id' => 'usuario_id']);
+    }
+}
