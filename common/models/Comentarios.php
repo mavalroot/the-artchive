d8c8190e4a4c3545a055932ccca27efb8b702bc4
mavalroot <mavalrodev@gmail.com>
2018-05-09 21:19:05 +0200

    Generado CRUD de comentarios con gii

---
+++ common/models/Comentarios.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comentarios".
+ *
+ * @property int $id
+ * @property int $usuario_id
+ * @property int $publicacion_id
+ * @property string $contenido
+ * @property int $comentario_id
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Comentarios $comentario
+ * @property Comentarios[] $comentarios
+ * @property Publicaciones $publicacion
+ * @property User $usuario
+ */
+class Comentarios extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'comentarios';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['usuario_id', 'publicacion_id', 'contenido'], 'required'],
+            [['usuario_id', 'publicacion_id', 'comentario_id'], 'default', 'value' => null],
+            [['usuario_id', 'publicacion_id', 'comentario_id'], 'integer'],
+            [['contenido'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['comentario_id'], 'exist', 'skipOnError' => true, 'targetClass' => Comentarios::className(), 'targetAttribute' => ['comentario_id' => 'id']],
+            [['publicacion_id'], 'exist', 'skipOnError' => true, 'targetClass' => Publicaciones::className(), 'targetAttribute' => ['publicacion_id' => 'id']],
+            [['usuario_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['usuario_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'usuario_id' => 'Usuario ID',
+            'publicacion_id' => 'Publicacion ID',
+            'contenido' => 'Contenido',
+            'comentario_id' => 'Comentario ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComentario()
+    {
+        return $this->hasOne(Comentarios::className(), ['id' => 'comentario_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComentarios()
+    {
+        return $this->hasMany(Comentarios::className(), ['comentario_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPublicacion()
+    {
+        return $this->hasOne(Publicaciones::className(), ['id' => 'publicacion_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsuario()
+    {
+        return $this->hasOne(User::className(), ['id' => 'usuario_id']);
+    }
+}
---
+++ frontend/views/comentarios/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Comentarios */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="comentarios-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'usuario_id')->textInput() ?>
+
+    <?= $form->field($model, 'publicacion_id')->textInput() ?>
+
+    <?= $form->field($model, 'contenido')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'comentario_id')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
