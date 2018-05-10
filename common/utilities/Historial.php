5e2309c388c8ae7ad5ce907022dad7d3010a6873
mavalroot <mavalrodev@gmail.com>
2018-05-10 21:55:12 +0200

    Arreglar heroku

--- common/utilities/Historial.php
+++ common/utilities/Historial.php
@@ -12,7 +12,7 @@ use common\models\ActividadReciente;
  */
 trait Historial
 {
-    public function crearHistorial($message, $url)
+    public static function crearHistorial($message, $url)
     {
         $actividad = new ActividadReciente();
         $actividad->mensaje = $message;
