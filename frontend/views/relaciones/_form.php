887cea42b8e47ccbf9e408cf05c237770a6921e3
mavalroot <mavalrodev@gmail.com>
2018-05-23 20:12:20 +0200

    opciones

--- frontend/views/relaciones/_form.php
+++ frontend/views/relaciones/_form.php
@@ -5,23 +5,38 @@ use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
 use common\models\TiposRelaciones;
 
+use kartik\select2\Select2;
+
 /* @var $this yii\web\View */
 /* @var $model common\models\Relaciones */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="relaciones-form">
+<div class="estandar-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
     <?= Html::hiddenInput('Relaciones[personaje_id]', $personaje->id) ?>
 
-    <?= $form->field($model, 'tipo_relacion_id')->dropDownList(ArrayHelper::map(TiposRelaciones::find()->all(), 'id', 'tipo'), ['prompt'=>'']) ?>
+    <?= $form->field($model, 'tipo_relacion_id')->widget(Select2::classname(), [
+        'data' => ArrayHelper::map(TiposRelaciones::find()->all(), 'id', 'tipo'),
+        'options' => ['placeholder' => 'Buscar al escribir...'],
+    ]); ?>
 
     <div id="contenido-relacion">
         <?= Html::button('Relación con personaje existente', ['class' => 'btn btn-sm btn-primary']) ?>
 
         <?= Html::button('Relación con personaje no registrado', ['class' => 'btn btn-sm btn-primary']) ?>
+
+        <?= $this->render('_existente', [
+            'model' => $model,
+            'form' => $form,
+        ]) ?>
+
+        <?= $this->render('_anonimo', [
+            'model' => $model,
+            'form' => $form,
+        ]) ?>
     </div>
 
     <div class="form-group">
--- frontend/views/relaciones/create.php
+++ frontend/views/relaciones/create.php
@@ -5,11 +5,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Relaciones */
 
-$this->title = 'Create Relaciones';
+$this->title = 'Añadir relación a ' . $personaje->nombre;
 $this->params['breadcrumbs'][] = ['label' => 'Relaciones', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="relaciones-create">
+<div class="estandar-action">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
