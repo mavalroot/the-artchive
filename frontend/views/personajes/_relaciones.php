f0f22a3918bfccc86da2f693595d86dda7840f00
mavalroot <mavalrodev@gmail.com>
2018-05-24 18:45:25 +0200

    Adelanto botón de borrar

--- frontend/views/personajes/_relaciones.php
+++ frontend/views/personajes/_relaciones.php
@@ -6,13 +6,18 @@ use yii\helpers\Html;
 <h2>Relaciones</h2>
 
 <?php foreach ($relaciones as $relacion): ?>
-    <?= $relacion->relacion ?>:
-    <?php if ($relacion->referencia): ?>
-        <?= Html::a(Yii::$app->formatter->asText($relacion->supj), ['personajes/view', 'id' => $relacion->supjid]) ?>
-        <?php if (!$relacion->aceptada): ?>
-            (Pendiente de confirmación)
+    <div class="relaciones-relacion">
+        <?= $relacion->relacion ?>:
+        <?php if ($relacion->referencia): ?>
+            <?= Html::a(Yii::$app->formatter->asText($relacion->supj), ['personajes/view', 'id' => $relacion->supjid]) ?>
+            <?php if (!$relacion->aceptada): ?>
+                (Pendiente de confirmación)
+            <?php endif; ?>
+        <?php else: ?>
+            <?= Yii::$app->formatter->asText($relacion->nombre) ?>
         <?php endif; ?>
-    <?php else: ?>
-        <?= Yii::$app->formatter->asText($relacion->nombre) ?>
-    <?php endif; ?>
+        <?php if ($model->isMine()): ?>
+            <input type="button" class="btn btn-xs btn-danger" name="" value="Borrar">
+        <?php endif; ?>
+    </div>
 <?php endforeach; ?>
