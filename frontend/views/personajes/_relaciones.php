8074ca9f4ba8ea50610674aed29c0c6d0b639995
mavalroot <mavalrodev@gmail.com>
2018-05-24 19:11:46 +0200

    corrección

--- frontend/views/personajes/_relaciones.php
+++ frontend/views/personajes/_relaciones.php
@@ -5,18 +5,18 @@ use yii\helpers\Html;
 
 <h2>Relaciones</h2>
 
-<?php foreach ($relaciones as $relacion): ?>
+<?php foreach ($relaciones as $relacion) : ?>
     <div class="relaciones-relacion">
         <?= $relacion->relacion ?>:
-        <?php if ($relacion->referencia): ?>
+        <?php if ($relacion->referencia) : ?>
             <?= Html::a(Yii::$app->formatter->asText($relacion->supj), ['personajes/view', 'id' => $relacion->supjid]) ?>
-            <?php if (!$relacion->aceptada): ?>
+            <?php if (!$relacion->aceptada) : ?>
                 (Pendiente de confirmación)
             <?php endif; ?>
-        <?php else: ?>
+        <?php else : ?>
             <?= Yii::$app->formatter->asText($relacion->nombre) ?>
         <?php endif; ?>
-        <?php if ($model->isMine()): ?>
+        <?php if ($model->isMine()) : ?>
             <?= Html::a('Borrar', ['/relaciones/delete', 'id' => $relacion->id], [
                 'class' => 'btn btn-sm btn-danger',
                 'data' => [
