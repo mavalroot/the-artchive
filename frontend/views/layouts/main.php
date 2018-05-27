3c828e363c903a4bd29992d44d4b6146dee7430d
mavalroot <mavalrodev@gmail.com>
2018-05-27 21:00:51 +0200

    ligera modificación

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -73,13 +73,13 @@ AppAsset::register($this);
             ],
         ];
     }
-    $menuItemsRight[] = '<li class="navbar-text"><div id="change-language">
+    $language = '<span class="navbar-text navbar-right"><div id="change-language">
     <button title="Change language to english" alt="Change language to english" type="button" class="flag-button' . (Yii::$app->language ==  'en-EN' ? ' flag-selected' : '') .
     '" value="en-EN"><span class="flag-icon flag-icon-gb"></span></button>
     <button title="Cambiar idioma a español" alt="Cambiar idioma a español"  type="button" class="flag-button' . (Yii::$app->language ==  'es-ES' ? ' flag-selected' : '') . '" value="es-ES">
     <span class="flag-icon flag-icon-es"></span>
     </button>
-    </div></li>';
+    </div></span>';
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-left'],
         'encodeLabels' => false,
@@ -91,6 +91,7 @@ AppAsset::register($this);
         'items' => $menuItemsRight,
     ]);
     ?>
+    <?= $language ?>
     <?php if (!Yii::$app->user->isGuest): ?>
         <ul id="w1" class="navbar-nav navbar-right nav">
                    <li>
