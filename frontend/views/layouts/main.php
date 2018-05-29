f39a1cee744c53e1f22a17057df1b405d3223f6c
mavalroot <mavalrodev@gmail.com>
2018-05-29 16:04:27 +0200

    Footer

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -127,30 +127,31 @@ AppAsset::register($this);
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                <h2 class="logo"><a href="#"> LOGO </a></h2>
+                <h2 class="logo"><a href="/"> THE ARTCHIVE </a></h2>
             </div>
+
             <div class="col-sm-3">
-                <h5>Get started</h5>
+                <h5><?= Yii::t('frontend', 'Sobre nosotros') ?></h5>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Sign up</a></li>
-                    <li><a href="#">Downloads</a></li>
+                    <li><a href="/site/about"><?= Yii::t('frontend', 'Sobre nosotros') ?></a></li>
+                    <li><a href="/site/contact"><?= Yii::t('frontend', 'Contactar') ?></a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Funcionamiento') ?></a></li>
                 </ul>
             </div>
             <div class="col-sm-3">
-                <h5>About us</h5>
+                <h5><?= Yii::t('frontend', 'Colabora') ?></h5>
                 <ul>
-                    <li><a href="#">Company Information</a></li>
-                    <li><a href="#">Contact us</a></li>
-                    <li><a href="#">Reviews</a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Sugerencias en general') ?></a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Sugerencias de traducción') ?></a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Proponer rasgos al azar') ?></a></li>
                 </ul>
             </div>
             <div class="col-sm-3">
-                <h5>Support</h5>
+                <h5><?= Yii::t('frontend', 'Soporte') ?></h5>
                 <ul>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Help desk</a></li>
-                    <li><a href="#">Forums</a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Foro') ?></a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Preguntas frecuentes') ?></a></li>
+                    <li><a href="#"><?= Yii::t('frontend', 'Términos y condiciones de uso') ?></a></li>
                 </ul>
             </div>
         </div>
