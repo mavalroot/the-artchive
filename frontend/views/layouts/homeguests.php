0fbe5aa6d1986d98913ea647681c355e09824aae
mavalroot <mavalrodev@gmail.com>
2018-06-02 16:01:39 +0200

    Partículas

--- frontend/views/layouts/homeguests.php
+++ frontend/views/layouts/homeguests.php
@@ -19,20 +19,31 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/homeguests.css">
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?= Alert::widget() ?>
-    <?= $content ?>
+    <div id="home-guest">
+        <div id="particles-js"></div>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
 </div>
-<footer>
-    Sobre Nosotros x Sobre The Artchive x Foro x Preguntas frecuentess x © <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?> by <a href="http://www.mavalroot.es/">mavalroot</a>
-
+<footer class="oneline">
+    <ul>
+        <li><a href="">Sobre Nosotros</a></li>
+        <li><a href="">Sobre The Artchive</a></li>
+        <li><a href="">Términos y condiciones</a></li>
+        <li><a href="">Preguntas frecuentes</a></li>
+        <li><a href="">Contactar</a></li>
+        <li><a href="">© <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?> by <a href="http://www.mavalroot.es/">mavalroot</a></a></li>
+    </ul>
 </footer>
-
+<script src="js/plugins/particles.min.js" charset="utf-8"></script>
+<script src="js/plugins/myparticles.js" charset="utf-8"></script>
 <?php $this->endBody() ?>
 </body>
 </html>
--- frontend/views/site/_guests.php
+++ frontend/views/site/_guests.php
@@ -1,32 +1,37 @@
-<div class="container-fluid" style="background: linear-gradient(-90deg, #a367a6, #7967a6); height: 100%;">
-<div class="row text-right">
-    <input type="text" name="" value="" placeholder="usuario">
-    <input type="password" name="" value="" placeholder="contraseña">
-    <input type="button" name="" value="Entrar">
-</div>
-<div class="row text-right">
-    <a href="">¿Olvidaste tu contraseña?</a>
-</div>
-
-<div class="row">
-    <ul>
-        <li><i class="fas fa-pencil-alt"></i> Crear contenido</li>
-        <li><i class="fas fa-users"></i> Conecta con la gente</li>
-        <li><i class="fas fa-paper-plane"></i> Comparte tus creaciones</li>
-    </ul>
+<div id="login" class="container-fluid">
+    <div class="row text-right form-inline">
+        <input class="form-control" type="text" name="" value="" placeholder="usuario">
+        <input class="form-control" type="password" name="" value="" placeholder="contraseña">
+        <input class="btn btn-secondary" type="button" name="" value="Entrar">
+    </div>
+    <div class="row text-right">
+        <a href="">¿Olvidaste tu contraseña?</a>
+    </div>
 </div>
 
-<div class="row">
-    <img src="http://placehold.it/100" alt="Logo">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    <h3>Únete</h3>
-    <ul>
-        <li>
-            <i class="fas fa-user-plus"></i> Registrarse
-        </li>
-        <li>
-            <i class="fas fa-sign-in-alt"></i> Conectarse
-        </li>
-    </ul>
-</div>
+<div class="myflex">
+    <div id="summary" class="row">
+        <ul>
+            <li><i class="fas fa-pencil-alt"></i> Crea contenido</li>
+            <li><i class="fas fa-users"></i> Conecta con la gente</li>
+            <li><i class="fas fa-paper-plane"></i> Comparte tus creaciones</li>
+        </ul>
+    </div>
+
+    <div id="description" class="row">
+        <img src="http://placehold.it/100" alt="Logo"><br />
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+
+    <div id="join" class="row">
+        <h3>Únete</h3>
+        <ul>
+            <li>
+                <a class="join-button" href="/site/login"><i class="fas fa-user-plus"></i> Registrarse</a>
+            </li>
+            <li>
+                <a class="join-button" href="/site/signup"><i class="fas fa-sign-in-alt"></i> Conectarse</a>
+            </li>
+        </ul>
+    </div>
 </div>
