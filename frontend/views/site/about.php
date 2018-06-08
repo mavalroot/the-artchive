74c8f61ac1c6707f9cf712f7e1deb61284961282
mavalroot <mavalrodev@gmail.com>
2018-06-08 21:09:46 +0200

    About para traducir

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -11,13 +11,13 @@ $this->title = Yii::t('frontend', 'Sobre nosotros');
         <h1><?= Html::encode($this->title) ?></h1>
         <div class="speech-bubble">
             <p itemprop="description">
-                <span itemprop="brand">The Artchive</span> nace como un proyecto de fin de curso, ¿la idea? Reunir en un mismo espacio, a modo de red social, a creadores de contenido que quieran compartir sus creaciones con otros.
+                <?= Yii::t('frontend/long', '<span itemprop="brand">The Artchive</span> nace como un proyecto de fin de curso, ¿la idea? Reunir en un mismo espacio, a modo de red social, a creadores de contenido que quieran compartir sus creaciones con otros.'); ?>
             </p>
             <p>
-                Su creador es <span itemprop="name"><?= Yii::$app->params['adminName'] ?></span>.
+                <?= Yii::t('frontend', 'Su creador es') ?> <span itemprop="name"><?= Yii::$app->params['adminName'] ?></span>.
             </p>
             <p>
-                The Artchive puede estar <span itemprop="address">en cualquier parte</span>. Y si quieres contactar con nosotros puedes hacerlo a través de <span itemprop="telephone"><a href="/site/contact">Contactar</a></span>.
+                <?= Yii::t('frontend/long', 'The Artchive puede estar <span itemprop="address">en cualquier parte</span>. Y si quieres contactar con nosotros puedes hacerlo a través de <span itemprop="telephone"><a href="/site/contact">Contactar</a></span>.'); ?>
             </p>
         </div>
         <div class="text-right">
