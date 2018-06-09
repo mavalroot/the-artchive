a66af5e16d29d6ae674e2a4593b6d245e14a38eb
mavalroot <mavalrodev@gmail.com>
2018-06-09 16:19:45 +0200

    Ya vuelve a generar los pdf

--- frontend/web/js/guardarpdf.js
+++ frontend/web/js/guardarpdf.js
@@ -1,26 +1,25 @@
-
-
 function guardar(nombre) {
-  var doc = new jsPDF('p', 'pt');
-  var columns = [
-    {title: "Título", dataKey: "title"},
-    {title: "Contenido", dataKey: "content"}
-  ];
-  var data = [];
-  let tr = $('table tbody tr');
-  $.each(tr, function(index, value) {
-      if (index < tr.length-1) {
-          data.push({title: value.cells[0].innerText, content: value.cells[1].innerText})
-      }
-  });
-  header(doc, nombre);
-  table(doc, columns, data);
-  copyright(doc);
-  doc.save(nombre + '.pdf');
+    var doc = new jsPDF('p', 'pt');
+    var columns = [
+        {title: "Título", dataKey: "title"},
+        {title: "Contenido", dataKey: "content"}
+    ];
+    var data = [];
+    let titulo = $('.personaje-body > h3');
+    let contenido = $('.personaje-body > .contenido');
+
+    $.each(titulo, function(index, value) {
+        data.push({title: titulo[index].innerText, content: contenido[index].innerText})
+    });
+
+    header(doc, nombre);
+    table(doc, columns, data);
+    copyright(doc);
+    doc.save(nombre + '.pdf');
 }
 
 function header(doc, nombre) {
-  doc.autoTableSetDefaults({
+    doc.autoTableSetDefaults({
         addPageContent: function(data) {
             doc.setFontSize(20);
             doc.text(nombre, data.settings.margin.left, 40);
@@ -32,12 +31,12 @@ function header(doc, nombre) {
 function copyright(doc) {
     var baseUrl = window.location.origin;
     doc.setFontSize(10);
-    doc.text(40,doc.autoTableEndPosY()+20, '© Artchive ' + (new Date()).getFullYear() + ' || ' + baseUrl);
+    doc.text(40,doc.autoTableEndPosY()+20, '© The Artchive ' + (new Date()).getFullYear() + ' || ' + baseUrl);
 }
 
 function table(doc, columns, data) {
-  doc.autoTable(columns, data, {
-      border: true,
+    doc.autoTable(columns, data, {
+        border: true,
         showHeader: 'never',
         columnStyles: {
             title: {fillColor: [60, 60, 60], textColor: 255, fontStyle: 'bold'},
@@ -45,7 +44,8 @@ function table(doc, columns, data) {
         },
         styles: {
             overflow: 'linebreak',
-            columnWidth: 'wrap'},
+            columnWidth: 'wrap'
+        },
     });
 }
 
