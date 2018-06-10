34d129aaf817b29f04110b2f026dbd2a724b100c
mavalroot <mavalrodev@gmail.com>
2018-06-10 18:19:58 +0200

    Quito propiedades no permitidas de button

--- frontend/views/layouts/_smallfooter.php
+++ frontend/views/layouts/_smallfooter.php
@@ -1,9 +1,9 @@
 <?php
 use yii\helpers\Html;
 
-$language = '<div id="change-language"><button title="Change language to english" alt="Change language to english" type="button" class="flag-button' . (Yii::$app->language ==  'en-EN' ? ' flag-selected' : '') .
+$language = '<div id="change-language"><button title="Change language to english" type="button" class="flag-button' . (Yii::$app->language ==  'en-EN' ? ' flag-selected' : '') .
 '" value="en-EN"><span class="flag-icon flag-icon-gb"></span></button>
-<button title="Cambiar idioma a español" alt="Cambiar idioma a español"  type="button" class="flag-button' . (Yii::$app->language ==  'es-ES' ? ' flag-selected' : '') . '" value="es-ES">
+<button title="Cambiar idioma a español" type="button" class="flag-button' . (Yii::$app->language ==  'es-ES' ? ' flag-selected' : '') . '" value="es-ES">
 <span class="flag-icon flag-icon-es"></span>
 </button>
 </div>';
